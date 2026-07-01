ac9c68255ac2e3ad4d0a5badc648cb5b6e73408e
Vívian Faria <164935075+Vivian-Faria@users.noreply.github.com>
2026-07-01 16:49:28 -0300

    perf: keep-alive ping na funcao Netlify a cada 4min para eliminar cold start

--- main.jsx
+++ main.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+// Keep-alive: pinga a funcao a cada 4min para evitar cold start da Netlify Function
+const ping = () => fetch("/.netlify/functions/query").catch(() => {});
+ping();
+setInterval(ping, 4 * 60 * 1000);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
